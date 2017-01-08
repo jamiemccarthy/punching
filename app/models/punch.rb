a0612f7f4798829cdee708bb16e673c4b1585f7c
Jamie McCarthy <jamie@mccarthy.vg>
2017-01-08 16:33:35 -0500

    Update Punch enums

--- app/models/punch.rb
+++ app/models/punch.rb
@@ -1,2 +1,16 @@
 class Punch < ApplicationRecord
+  enum :site      => { :imgur => 0 }
+  enum :direction => { :down => 0, :up => 1 }
+
+  def site_domain
+    if imgur?
+      "imgur.com"
+    end
+  end
+
+  def url
+    if imgur?
+      URI("http://#{site_domain}/gallery/#{site_id}")
+    end
+  end
 end
